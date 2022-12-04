43976e20a553f9cd9f4c41db260d2658d300f9cb
farrukhjon <farrukhjonuktamov0409@gmail.com>
2022-12-04 17:55:23 +0500

    new js file

--- 12js.js
+++ 12js.js
@@ -1,1 +1,77 @@
-alert("bos gazini");
+
+//13 dars
+
+// var x=10,y=4;
+// if (x<y) {
+//     console.log("kettuuuu");
+// } else {
+//     console.log("qolluuuu");
+// }
+
+
+// 14 dars
+
+// // var n = 10;
+// // // while (n<=10) {
+// // // console.log(n);
+// // // n++;
+// // // }
+// // // do {
+// // // console.log(n); n++;
+// // // }while (n<=12);
+// // // for (let i=1; i<8; i++){
+// // //     console.log(n); n++;
+// // // }
+// // for (var i=0; i<10; i++){ if (i===7){continue;}
+// //     console.log(i);
+// // }
+
+
+// 15dars
+
+// var numberOfSeries = +prompt("Nechta serial ko'rdingiz?", '');
+
+// var seriesDB = {
+//   count: numberOfSeries,
+//   series: {},
+//   actors: {},
+//   genres: [],
+//   private: false,
+// };
+
+// for (var i = 0; i < 2; i++) {
+//   var a = prompt("Oxirgi ko'rgan serialingiz?"),
+//     b = prompt("Nechi baxo berasiz?");
+
+//   if (a != null && b != null && a != '' && b != '') {
+//     seriesDB.series[a] = b;
+//     console.log("done");
+//   } else {
+//     console.log("error");
+//     i--;
+//   }
+// }
+
+// if (seriesDB.count < 5) {
+//   console.log("Kam serial ko'ripsiz");
+// } else if (seriesDB.count >= 5 && seriesDB.count < 10) {
+//   console.log("Siz classik tamoshabin ekansiz");
+// } else if (seriesDB.count >= 10) {
+//   console.log("Siz serialchi zvezda ekansiz");
+// } else {
+//   console.log("Error");
+// }
+
+// console.log(seriesDB);
+
+
+// 16dars
+
+function name(params) {
+    
+}
+
+
+
+
+
---
+++ js_jskettu.js
@@ -0,0 +1,33 @@
+// Qiymati string bo'lgan Object
+var namen = "Umar";
+
+// Qiymati raqam bo'lgan Object
+let number = 12;
+
+// Qiymati boolean bo'lgan Object
+let isCorrect = true;
+
+// Qiymati boshqa Object bo'lgan Object
+const car = {
+    make: 'Tesla',
+    model: 'Model X',
+    year: 2020
+};
+
+// Qiymati funksiya bo'lgan object
+const greeting = function(){
+   return ('Assalamu alaykum!');
+}
+book = new Object();{
+book.name = 'Deep work';
+book.author = 'Cale Newport';
+book.year = 2016;
+console.log(book);
+}
+console.log(namen);
+console.log(car);
+console.log(isCorrect);
+console.log(number);
+
+
+
